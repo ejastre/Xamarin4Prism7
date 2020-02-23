2fedda12a5cdb398acf13d29fb25dd917efb349c
ejastre <edgar.jastre@gmail.com>
2020-02-23 18:04:33 +0000

    .

--- XamarinPrism7/XamarinPrism7/Helper/Config.cs
+++ XamarinPrism7/XamarinPrism7/Helper/Config.cs
@@ -2,7 +2,7 @@
 {
     public static class Config
     {
-        public const string FirebaseKey = "AIzaSyB9Y4J2aKFBFLRL4dxdLBl8w3l-mby1beg";
+        public const string FirebaseKey = "";
 
         public const string FirebaseUrl = "https://xamarin4prism7.firebaseio.com";
     }
